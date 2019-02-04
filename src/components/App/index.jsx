3af262bfbd99d09d91a81fa9613532c6f88104eb
Ricardo Ferreira <thegiantbeast.pt@gmail.com>
2019-02-04 03:05:00 +0000

    Add initial app structure

---
+++ src/components/App/index.jsx
@@ -0,0 +1,7 @@
+import React, {Component} from 'react'
+
+export default class App extends Component {
+    render() {
+        return <h1>Books List App</h1>
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+	entry: ['@babel/polyfill', './src/index.jsx'],
+	output: {
+		path: __dirname + '/dist',
+		publicPath: '/',
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: ['babel-loader']
+			},
+			{
+				test: /\.html$/,
+				use: ['html-loader']
+			}
+		]
+	},
+	resolve: {
+		extensions: ['*', '.js', '.jsx']
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: 'src/index.html'
+		}),
+		new webpack.HotModuleReplacementPlugin()
+	],
+	devServer: {
+		contentBase: './assets',
+		hot: true
+	}
+}
